29ed86041f69ad0644087fef4494e2b73bec52c9
otogan <onurtogan10@gmail.com>
2020-01-21 08:13:56 -0600

    update

---
+++ reservations/src/main/java/com/onurtogan/landon/data/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.onurtogan.landon.data.repository;
+
+import com.onurtogan.landon.data.entity.Room;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room, Long>{
+    Room findByNumber(String number);
+}
